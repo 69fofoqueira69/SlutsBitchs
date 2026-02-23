11118bee363b3d3ba855604e8073228579d66253
69fofoqueira69 <69fofoqueira69@gmail.com>
2026-02-22 21:50:42 -0300

    chore: configurar merge do profiles.json para evitar conflitos de pull

---
+++ src/pages/profile.js
@@ -0,0 +1,20 @@
+import { renderProfileDetails } from '../components/profileDetails.js';
+import { getProfileById } from '../data/repository.js';
+import { buildComputedProfile } from '../data/tagRules.js';
+
+const profileRoot = document.querySelector('#profile-root');
+
+async function init() {
+  try {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+    const profile = id ? await getProfileById(id) : null;
+
+    renderProfileDetails(profileRoot, profile ? buildComputedProfile(profile) : null);
+  } catch (error) {
+    profileRoot.innerHTML = '<p>Erro ao carregar perfil.</p>';
+    console.error(error);
+  }
+}
+
+init();
